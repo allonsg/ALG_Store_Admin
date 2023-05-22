ed3b14d1e38b3112da2ed7d3b6283e29661a7930
allonsg <109518407+allonsg@users.noreply.github.com>
2023-05-22 21:53:01 +0300

    created admin auth

---
+++ lib/auth.ts
@@ -0,0 +1,14 @@
+import type { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "./mongodb";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID || "",
+      clientSecret: process.env.GOOGLE_SECRET || "",
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+};
